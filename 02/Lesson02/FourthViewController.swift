82a56792ebecc3b8a33e2f32aebaa9aaea9feb3a
Tony Yu <tonyyu093@gmail.com>
2015-03-10 13:21:23 +1100

    for the fourth view, used a class to contain the function for the calculation, also commented on relevant thoughts

--- 02/Lesson02/FourthViewController.swift
+++ 02/Lesson02/FourthViewController.swift
@@ -13,27 +13,16 @@ class FourthViewController: UIViewController {
     @IBOutlet weak var outputLabel: UILabel!
 /*
     TODO seven: Hook up the text input box, label and and a ‘calculate’ button. Create a ‘fibonacci adder’ class with a method ‘fibonacciNumberAtIndex' which takes indexOfFibonacciNumber (an integer).  When the button is pressed, create an instance of that class, call the method, and print out the appropriate fibonacci number of an inputted integer.
+    
     The first fibonacci number is 0, the second is 1, the third is 1, the fourth is two, the fifth is 3, the sixth is 5, etc. The Xth fibonacci number is the sum of the X-1th fibonacci number and the X-2th fibonacci number.
 */
     @IBAction func displayXthFibonacciNumber(sender: AnyObject) {
-        if inputNumberField.text.toInt() != nil && inputNumberField.text.toInt() != Int() && inputNumberField.text.toInt() != 0{
-            var result : Int = 0
-            var previousResult : Int = 0
-            
-            
-            for var i = 0; i < inputNumberField.text.toInt(); i++ {
-                
-                if i < 2 {
-                    result = i
-                } else {
-                    var resultHolder : Int
-                    resultHolder = result
-                    result = result + previousResult
-                    previousResult = resultHolder
-                }
-                
-            }
-            outputLabel.text = "\(result)"
+        var fibonacci = fibonacci_adder()
+        fibonacci.outputLabel = outputLabel
+        //passing the value of the outputLabel to the adder class so that label can be changed within that class
+        if inputNumberField.text.toInt() != nil && inputNumberField.text.toInt() != Int() {
+            //avoiding edge cases
+            fibonacci.fibonacciNumberAtIndex(inputNumberField.text.toInt()!)
         }
     }
 }
---
+++ 02/Lesson02/fibonacci adder.swift
@@ -0,0 +1,44 @@
+//
+//  fibonacci adder.swift
+//  Lesson02
+//
+//  Created by Tony Yu on 9/03/2015.
+//  Copyright (c) 2015 General Assembly. All rights reserved.
+//
+
+import UIKit
+
+class fibonacci_adder : NSObject{
+
+    weak var outputLabel : UILabel!
+    //to associate with the label from the FourthViewController class so it could change the label from this class
+    
+    func fibonacciNumberAtIndex (indexOfFibonacciNumber : Int) {
+        //would have loved to have a return value for this function, but since it is specified, the action to change the label is done here
+
+        if indexOfFibonacciNumber != 0{
+            var result : Int = 0
+            var previousResult : Int = 0
+            
+            
+            for var i = 0; i < indexOfFibonacciNumber; i++ {
+                
+                if i < 2 {
+                    result = i
+                    //had to put in special consideration for 1st and 2nd iteration as they do not have a previous value to use
+                } else {
+                    var resultHolder : Int
+                    resultHolder = result
+                    result = result + previousResult
+                    previousResult = resultHolder
+                }
+                
+            }
+            if var label = outputLabel {
+                label.text! = "\(result)"
+                //safety check and changing the label
+            }
+        }
+    }
+
+}

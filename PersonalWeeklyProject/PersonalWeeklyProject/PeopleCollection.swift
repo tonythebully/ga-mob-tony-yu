a55e5d3bb4f9895e7d20dbd47a1974f87495c02c
Tony Yu <tonyyu093@gmail.com>
2015-03-28 17:42:17 +1100

    gave the people a type

--- PersonalWeeklyProject/PersonalWeeklyProject/PeopleCollection.swift
+++ PersonalWeeklyProject/PersonalWeeklyProject/PeopleCollection.swift
@@ -9,11 +9,24 @@
 import Foundation
 
 class PeopleCollection {
-    var peopleArray = [
+    var peopleArray : [Dictionary<String, AnyObject>] = [
         [
             "name" : "add a new contact!",
             "info" : ["Where would you stalk them?", "Perhaps youtube links, home address, etc"]
         ]
     ]
     
+    // appends a new dictionary with new contact to peopleArray
+    func addNewContact(name : String) {
+        let newPersonDict : Dictionary<String, AnyObject> = [
+            "name" : name,
+            // leaving the info empty as it will be appending at another stage
+            "info" : [""]
+        ]
+        self.peopleArray.append(newPersonDict)
+    }
+    // changes the info related to the current person via indexPath
+    func editPersonInfo (info : [String], indexPath : NSIndexPath) {
+        self.peopleArray[indexPath.row]["info"] = info
+    }
 }

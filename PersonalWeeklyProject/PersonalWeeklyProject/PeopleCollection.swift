6e15df8b8c7eb372e5880e7c3332127601e9f7d9
Tony Yu <tonyyu093@gmail.com>
2015-04-21 16:33:37 +1000

    the class to coredata should be complete

--- PersonalWeeklyProject/PersonalWeeklyProject/PeopleCollection.swift
+++ PersonalWeeklyProject/PersonalWeeklyProject/PeopleCollection.swift
@@ -17,28 +17,44 @@ class PeopleCollection {
     
     let profileFetchRequest = NSFetchRequest(entityName: "Entity")
     let sortDescriptor = NSSortDescriptor(key: "dateAdded", ascending: true)
-    profileFetchRequest.sortDescriptors = [sortDescriptor]
+//    profileFetchRequest.sortDescriptors = [sortDescriptor]
+
     
     func appendNewProfile(name: String) {
         
+        // creates a new profile with no mediums
         let newProfile = NSEntityDescription.insertNewObjectForEntityForName("Entity", inManagedObjectContext: self.managedObjectContext) as! Entity
         newProfile.name = name
+        newProfile.mediums = []
         newProfile.dateAdded = NSDate()
     }
     
     func appendingProfileDetails(medium: String, indexPathOfProfile: NSIndexPath) {
         
-        let changes = self.managedObjectContext.executeFetchRequest(self.profileFetchRequest, error: nil) as! [Entity]
-        changes[indexPathOfProfile.row].mediums.append(medium)
+        // goes to specific profile and append a medium attached to that profile
+        self.profileFetchRequest.sortDescriptors = [self.sortDescriptor]
+        
+        let profileToChange = self.managedObjectContext.executeFetchRequest(self.profileFetchRequest, error: nil) as! [Entity]
+        profileToChange[indexPathOfProfile.row].mediums.append(medium)
     }
     
     func deleteProfile(indexPathofProfile: NSIndexPath) {
+
+        // goes to specific profile and delete the lot
+        self.profileFetchRequest.sortDescriptors = [self.sortDescriptor]
         let profileToDelete = self.managedObjectContext.executeFetchRequest(self.profileFetchRequest, error: nil) as! [Entity]
         
         self.managedObjectContext.deleteObject(profileToDelete[indexPathofProfile.row])
     }
+    
+    
     func deleteMediumofProfile(indexPathOfProfile: NSIndexPath, indexPathOfMedium: NSIndexPath) {
+    
+        //goes to the specific profile, then to delete specific medium of that profile
+        self.profileFetchRequest.sortDescriptors = [self.sortDescriptor]
+        let profileToChange = self.managedObjectContext.executeFetchRequest(self.profileFetchRequest, error: nil) as! [Entity]
         
+        profileToChange[indexPathOfProfile.row].mediums.removeAtIndex(indexPathOfMedium.row)
     }
     
 }
